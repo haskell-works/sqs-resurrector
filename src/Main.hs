09e78f834964daf5e65442305eefefe2c68c4a6b
Alexey Raga <alexey.raga@gmail.com>
2015-12-01 17:25:56 +1100

    Init

---
+++ src/Main.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Control.Lens
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.AWS
+import           Data.Monoid
+import           Data.Text               (Text)
+import qualified Data.Text               as Text
+import qualified Data.Text.IO            as Text
+import           Network.AWS.SQS         as SQS
+import           System.IO
+
+queueName = "allen-batched-attacks-dead-letter"
+
+main :: IO ()
+main = do
+  log <- newLogger Debug stdout
+  env <- newEnv Oregon Discover <&> envLogger .~ log
+
+  let say = liftIO . Text.putStrLn
+
+  runResourceT . runAWST env $ do
+    url <- view gqursQueueURL <$> send (getQueueURL queueName)
+    src <- view ldlsqrsQueueURLs <$>  send (listDeadLetterSourceQueues $ url)
+    mss <- send (receiveMessage url & rmWaitTimeSeconds ?~ 20)
+    forM_ (mss ^. rmrsMessages) $ \m -> do
+            say "---------------------------------------------"
+            say $ "Received Message: " <> Text.pack (show m)
+    --forM_ src say
+    --say uuu
+    say "End."
+
+
